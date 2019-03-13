7865b899ffcdf1e14ee68a2fdd73a3bfea0eeb50
Alex Souza <aasouzaconsult@gmail.com>
2019-03-12 23:39:43 -0300

    Scripts de execução (Escola de Verão MACC 2019)

--- AmbienteBI.sql
+++ AmbienteBI.sql
@@ -17,14 +17,14 @@ IF (USER_ID('Teste_DDM') IS NULL)
     
 GRANT SELECT ON dbo.TbPessoa TO [Teste_DDM]
 
-Select * From TbPessoa;
+Select * From TbPessoa; -- meu usuário
 
 -- Visualizando os dados mascarados (Como se fosse o usuário Teste_DDM, que acabamos de criar)
 EXECUTE AS USER = 'Teste_DDM'
 GO
 SELECT * FROM TbPessoa
 GO
-REVERT -- Reverte as permissões para o seu usuário
+REVERT -- Reverte as permissões para o meu usuário
 GO
 
 ------------------------------------------------------------------------------------------------
@@ -42,7 +42,7 @@ GO
 -- Business Intelligence --
 ---------------------------
 -- Criando o banco de dados para o BI (Boa prática)
-CREATE DATABASE ArmazemDeDados
+-- CREATE DATABASE ArmazemDeDados (já criamos na prática anterior)
 USE ArmazemDeDados;
 
 --------------------------------------------
@@ -60,7 +60,7 @@ Select Ven.Cod
      , Ven.[Vr Unitário]
      , Ven.Total
      , Ven.Estado
-     , Ven.Cidade
+     , Municipio = Ven.Cidade
      , Vdd.Vendedor
      , Vdd.VrBonus as TaxaComissao
      , Comissao = (Ven.Total*Vdd.VrBonus)/100
@@ -72,18 +72,18 @@ Select Ven.Cod
 -- ABRIR NO POWERBI
 --> ExemploPowerBI_ConhecimentoESabedoria.pbix
 
--- Automático*
+-- Procedure para Carga de Dados
 
 USE ArmazemDeDados
 
-CREATE PROCEDURE [dbo].[spCarga_Dados] AS 
+ALTER PROCEDURE [dbo].[spCarga_Dados] AS 
 BEGIN
 
   -- Vendas
   IF EXISTS (SELECT * FROM ArmazemDeDados.sys.objects WHERE object_id = OBJECT_ID(N'ArmazemDeDados..Visao_Vendas') AND type IN (N'U'))
         DROP TABLE ArmazemDeDados..Visao_Vendas
   
-    Select Ven.Cod 
+    Select CodigoVenda = Convert(varchar, Ven.Cod) 
          , Ven.[Data Venda]
          , Ven.Cliente
          , Pro.Produto
@@ -92,7 +92,7 @@ BEGIN
          , Ven.[Vr Unitário]
          , Ven.Total
          , Ven.Estado
-         , Ven.Cidade
+         , Municipio = Ven.Cidade
          , Vdd.Vendedor
          , Vdd.VrBonus as TaxaComissao
          , Comissao = (Ven.Total*Vdd.VrBonus)/100
@@ -102,27 +102,86 @@ BEGIN
       Left Join BancoSistemaVendas..TbVendedor Vdd on Vdd.CodigoVendedor = Ven.CodigoVendedor -- 2 Vendas sem vendedor
 
   -- Vendedor / Dependentes
-  IF EXISTS (SELECT * FROM ArmazemDeDados.sys.objects WHERE object_id = OBJECT_ID(N'ArmazemDeDados..Visao_DependenteVendedor') AND type IN (N'U'))
-        DROP TABLE ArmazemDeDados..Visao_DependenteVendedor
+  IF EXISTS (SELECT * FROM ArmazemDeDados.sys.objects WHERE object_id = OBJECT_ID(N'ArmazemDeDados..Visao_VendedorDependente') AND type IN (N'U'))
+        DROP TABLE ArmazemDeDados..Visao_VendedorDependente
 
-    Select Ven.Vendedor
+    Select CodigoVendedor = Convert(varchar, Ven.CodigoVendedor)
+	     , Ven.Vendedor
          , Pes.Pessoa
          , Pes.Sexo
          , Pes.Endereco
          , Pes.Cidade
          , Pes.EMail
          , Pes.Numero_Cartao
+		 , TempoEmpresa = DATEDIFF(year, Pes.DtAdmissao, getdate())
          , DtAdmissao
          , DtDemissao
+		 , CodigoDependente = Convert(varchar, Dep.CodigoDependente)
          , Dep.Depedente
-         , [Sexo Dependente] = CASE WHEN Dep.Sexo = 1 THEN 'M' ELSE 'F' END
+         , SexoDependente = CASE WHEN Dep.Sexo = 1 THEN 'M' 
+	                             WHEN Dep.Sexo = 2 THEN 'F'
+	                        ELSE 'Erro' END
+		 , IdadeDependente = DATEDIFF(year, Dep.DtNasc, getdate())
       INTO ArmazemDeDados..Visao_VendedorDependente
-      From BancoSistemaVendas..TbVendedor   Ven
-      Join BancoSistemaVendas..TbPessoa     Pes ON Pes.CodigoPessoa   = Ven.CodigoPessoa
+      From BancoSistemaVendas..TbVendedor        Ven
+      Join BancoSistemaVendas..TbPessoa          Pes ON Pes.CodigoPessoa   = Ven.CodigoPessoa
       Left Join BancoSistemaVendas..TbDependente Dep ON Dep.CodigoVendedor = Ven.CodigoVendedor
      Order by Ven.CodigoVendedor
 
+  -- Vendas Completa
+  IF EXISTS (SELECT * FROM ArmazemDeDados.sys.objects WHERE object_id = OBJECT_ID(N'ArmazemDeDados..Visao_VendasCompleta') AND type IN (N'U'))
+        DROP TABLE ArmazemDeDados..Visao_VendasCompleta
+
+	Select CodigoVenda = Convert(varchar, Ven.Cod)
+         , Ven.[Data Venda]
+         , Ven.Cliente
+		 , CodigoProduto = Convert(varchar, Pro.Codigo)
+         , DescricaoProduto = Pro.Produto
+		 , TipoProduto      = Pro.Tipo
+         , Ven.Und
+         , Ven.Quantidade
+         , Ven.[Vr Unitário]
+         , Ven.Total
+         , Ven.Estado
+         , Ven.Cidade
+		 , CodigoVendedor       = Convert(varchar, Vdd.CodigoVendedor)
+         , ApelidoVendedor      = Vdd.Vendedor
+		 , NomeVendedor         = Pes.Pessoa
+		 , SexoVendedor         = Pes.Sexo
+		 , EnderecoVendedor     = Pes.Endereco
+		 , CidadeVendedor       = Pes.Cidade
+		 , EmailVendedor        = Pes.Email
+		 , CartaoVendedor       = Pes.Numero_Cartao
+		 , TempoEmpresaVendedor = DATEDIFF(year, Pes.DtAdmissao, getdate())
+		 , AdmissaoVendedor     = Pes.DtAdmissao
+		 , DemissaoVendedor     = Pes.DtDemissao
+         , TaxaComissaoVendedor = Vdd.VrBonus
+         , ComissaoVendedor     = (Ven.Total*Vdd.VrBonus)/100
+      INTO ArmazemDeDados..Visao_VendasCompleta
+      From BancoSistemaVendas..TbVendas          Ven
+      Join BancoSistemaVendas..TbProdutos        Pro on Pro.Codigo         = Ven.Codigo
+      Left Join BancoSistemaVendas..TbVendedor   Vdd on Vdd.CodigoVendedor = Ven.CodigoVendedor -- 2 Vendas sem vendedor
+	  Left Join BancoSistemaVendas..TbPessoa     Pes on Pes.CodigoPessoa   = Vdd.CodigoPessoa
+
 END
 
--- Rodando a Carga
+-- Rodando a Carga (Manual)
 EXEC [dbo].[spCarga_Dados]
+
+-- Job de Carga (Automático) - SQL Server
+-- Nome do Job: Carga_ArmazemDados
+
+-----------------------------------
+-- Teste de atualização de dados --
+-----------------------------------
+Select * From ArmazemDeDados..Visao_VendasCompleta
+-- observar o campo: DemissaoVendedor
+
+-- Vamos demitir a pessoa de código 1 (No sistema em "Produção")
+Update BancoSistemaVendas..TbPessoa
+Set DtDemissao = getdate()
+Where CodigoPessoa = 1
+
+-- Rodar o JOB (forçar)
+Select * From ArmazemDeDados..Visao_VendasCompleta
+-- observar novamente o campo: DemissaoVendedor
--- AmbienteSQLServer.sql
+++ AmbienteSQLServer.sql
@@ -81,37 +81,39 @@ CREATE TABLE TbDependente (
 -- INSERT (https://docs.microsoft.com/pt-br/sql/t-sql/statements/insert-transact-sql?view=sql-server-2017)
 
 -- Tabela Pessoa
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 1', 'M', 'Rua do teste 1', 'Eusébio', 'e-mail_x@e-mail.com', '1254125414562366', '20150512', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 2', 'M', 'Rua do teste 3', 'Euzebio', 'e-mail_y@e-mail.com', '1254125414562386', '20150515', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 3', 'F', 'Rua do teste 7', 'Fortaleza', 'e-mail_a@e-mail.com', '1254125414562266', '20140712', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 4', 'M', 'Rua do teste 2', 'Fortaleza', 'e-mail_v@e-mail.com', '1254125414562166', '20150612', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 5', 'M', 'Rua do teste 3', 'Eusébio', 'e-mail_c@e-mail.com', '1254125414562766', '20150810', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 6', 'M', 'Rua do teste 4', 'Maracanaú', 'e-mail_d@e-mail.com', '1254125414562366', '20170419', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 7', 'F', 'Rua do teste 5', 'Aquiraz', 'e-mail_j@e-mail.com', '1253125414562366', '20150705', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 8', 'M', 'Rua do teste 8', 'Fortalesa', 'e-mail_l@e-mail.com', '1244125414562366', '20130211', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 9', 'F', 'Rua do teste 1', 'Eusébio', 'e-mail_m@e-mail.com', '1253125414562346', '20180512', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 10', 'M', 'Rua do teste 03', 'Euzebio', 'e-mail_n@e-mail.com', '1254125414562366', '20150515', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 1' , 'M', 'Rua do teste 1' , 'Eusébio'  , 'e-mail_x@e-mail.com', '1254125414562366', '20150512', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 2' , 'M', 'Rua do teste 3' , 'Euzebio'  , 'e-mail_y@e-mail.com', '1254125414562386', '20150515', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 3' , 'F', 'Rua do teste 7' , 'Fortaleza', 'e-mail_a@e-mail.com', '1254125414562266', '20140712', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 4' , 'M', 'Rua do teste 2' , 'Fortaleza', 'e-mail_v@e-mail.com', '1254125414562166', '20150612', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 5' , 'M', 'Rua do teste 3' , 'Eusébio'  , 'e-mail_c@e-mail.com', '1254125414562766', '20150810', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 6' , 'M', 'Rua do teste 4' , 'Maracanaú', 'e-mail_d@e-mail.com', '1254125414562366', '20170419', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 7' , 'F', 'Rua do teste 5' , 'Aquiraz'  , 'e-mail_j@e-mail.com', '1253125414562366', '20150705', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 8' , 'M', 'Rua do teste 8' , 'Fortalesa', 'e-mail_l@e-mail.com', '1244125414562366', '20130211', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 9' , 'F', 'Rua do teste 1' , 'Eusébio'  , 'e-mail_m@e-mail.com', '1253125414562346', '20180512', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 10', 'M', 'Rua do teste 03', 'Euzebio'  , 'e-mail_n@e-mail.com', '1254125414562366', '20150515', null)
 INSERT INTO TbPessoa VALUES ('Nome da Pessoa 11', 'M', 'Rua do teste 17', 'Fortaleza', 'e-mail_o@e-mail.com', '1253125414562366', '20140712', null)
 INSERT INTO TbPessoa VALUES ('Nome da Pessoa 12', 'F', 'Rua do teste 22', 'Fortaleza', 'e-mail_p@e-mail.com', '1254125414562366', '20150612', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 13', 'M', 'Rua do teste 33', 'Eusébio', 'e-mail_q@e-mail.com', '1284125414562366', '20150810', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 13', 'M', 'Rua do teste 33', 'Eusébio'  , 'e-mail_q@e-mail.com', '1284125414562366', '20150810', null)
 INSERT INTO TbPessoa VALUES ('Nome da Pessoa 14', 'M', 'Rua do teste 34', 'Maracanaú', 'e-mail_r@e-mail.com', '1254125414562366', '20170419', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 15', 'F', 'Rua do teste 25', 'Aquiraz', 'e-mail_s@e-mail.com', '1244125414562366', '20150705', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 16', 'M', 'Rua do teste 18', 'Fotaleza', 'e-mail_t@e-mail.com', '1254125414562226', '20130211', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 15', 'F', 'Rua do teste 25', 'Aquiraz'  , 'e-mail_s@e-mail.com', '1244125414562366', '20150705', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 16', 'M', 'Rua do teste 18', 'Fotaleza' , 'e-mail_t@e-mail.com', '1254125414562226', '20130211', null)
 INSERT INTO TbPessoa VALUES ('Nome da Pessoa 17', 'M', 'Rua do teste 28', 'Fortaleza', 'e-mail_u@e-mail.com', '1454125414562266', '20170211', null)
-INSERT INTO TbPessoa VALUES ('Nome da Pessoa 18', 'F', 'Rua do teste 38', 'Fortaleza', 'e-mail_vr@e-mail.com', '1204125414561366', '20160211', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 18', 'F', 'Rua do teste 38', 'Fortaleza', 'e-mail_v@e-mail.com', '1204125414561366', '20160211', null)
 INSERT INTO TbPessoa VALUES ('Nome da Pessoa 19', 'M', 'Rua do teste 48', 'Fortaleza', 'e-mail_z@e-mail.com', '1294125414562266', '20160211', null)
+INSERT INTO TbPessoa VALUES ('Nome da Pessoa 20', 'H', 'Rua do teste 20', 'Fortaleza', 'e-mail_w@e-mail.com', '1294125414562266', '20162211', null)
+
 Select * From TbPessoa
 
 -- Tabela Vendedor
-INSERT INTO TbVendedor VALUES ('Vendedor 1', 10.00, 1)
-INSERT INTO TbVendedor VALUES ('Vendedor 2', 12.00, 2)
-INSERT INTO TbVendedor VALUES ('Vendedor 3', 13.00, 3)
-INSERT INTO TbVendedor VALUES ('Vendedor 4', 14.00, 4)
-INSERT INTO TbVendedor VALUES ('Vendedor 5', 15.00, 5)
-INSERT INTO TbVendedor VALUES ('Vendedor 6', 16.00, 6)
-INSERT INTO TbVendedor VALUES ('Vendedor 7', 17.00, 7)
-INSERT INTO TbVendedor VALUES ('Vendedor 8', 18.00, 8)
-INSERT INTO TbVendedor VALUES ('Vendedor 9', 10.00, 9)
+INSERT INTO TbVendedor VALUES ('Vendedor 1' , 10.00, 1)
+INSERT INTO TbVendedor VALUES ('Vendedor 2' , 12.00, 2)
+INSERT INTO TbVendedor VALUES ('Vendedor 3' , 13.00, 3)
+INSERT INTO TbVendedor VALUES ('Vendedor 4' , 14.00, 4)
+INSERT INTO TbVendedor VALUES ('Vendedor 5' , 15.00, 5)
+INSERT INTO TbVendedor VALUES ('Vendedor 6' , 16.00, 6)
+INSERT INTO TbVendedor VALUES ('Vendedor 7' , 17.00, 7)
+INSERT INTO TbVendedor VALUES ('Vendedor 8' , 18.00, 8)
+INSERT INTO TbVendedor VALUES ('Vendedor 9' , 10.00, 9)
 INSERT INTO TbVendedor VALUES ('Vendedor 10', 12.00, 10)
 INSERT INTO TbVendedor VALUES ('Vendedor 11', 13.00, 11)
 INSERT INTO TbVendedor VALUES ('Vendedor 12', 14.00, 12)
@@ -123,17 +125,19 @@ INSERT INTO TbVendedor VALUES ('Vendedor 17', 18.00, 17)
 INSERT INTO TbVendedor VALUES ('Vendedor 18', 18.00, 100) -- Erro de Integridade Relacional - Foreign Key
 INSERT INTO TbVendedor VALUES ('Vendedor 19', 18.00, 200) -- Erro de Integridade Relacional - Foreign Key
 Select * From TbVendedor
+Select * From TbPessoa
 
 -- Tabela Dependente
-INSERT INTO TbDependente VALUES (1, 'Dependente A', 1, '20150101')
-INSERT INTO TbDependente VALUES (3, 'Dependente B', 1, '20160601')
-INSERT INTO TbDependente VALUES (5, 'Dependente C', 2, '20170101')
-INSERT INTO TbDependente VALUES (7, 'Dependente D', 2, '20140501')
-INSERT INTO TbDependente VALUES (9, 'Dependente E', 1, '20150101')
-INSERT INTO TbDependente VALUES (11, 'Dependente F', 1, '20150101')
-INSERT INTO TbDependente VALUES (13, 'Dependente G', 2, '20150101')
+INSERT INTO TbDependente VALUES (1,  'Dependente A' , 1, '20150101')
+INSERT INTO TbDependente VALUES (3,  'Dependente B' , 1, '20160601')
+INSERT INTO TbDependente VALUES (5,  'Dependente C' , 2, '20170101')
+INSERT INTO TbDependente VALUES (7,  'Dependente D' , 2, '20140501')
+INSERT INTO TbDependente VALUES (9,  'Dependente E' , 1, '20150101')
+INSERT INTO TbDependente VALUES (11, 'Dependente F' , 1, '20150101')
+INSERT INTO TbDependente VALUES (13, 'Dependente G' , 2, '20150101')
 INSERT INTO TbDependente VALUES (1,  'Dependente AA', 1, '20181005')
-INSERT INTO TbDependente VALUES (13,  'Dependente GG', 1, '20171005')
+INSERT INTO TbDependente VALUES (13, 'Dependente GG', 1, '20171005')
+INSERT INTO TbDependente VALUES (12, 'Dependente HH', 3, '20171005')
 Select * From TbDependente
 
 -------------------------------------------
@@ -166,7 +170,29 @@ Select Ven.[Data Venda]
   join BancoSistemaVendas..TbProdutos      Pro on Pro.Codigo         = Ven.Codigo
   left join BancoSistemaVendas..TbVendedor Vdd on Vdd.CodigoVendedor = Ven.CodigoVendedor -- 1 Venda sem vendedor
   Order by Vdd.Vendedor
+
+Select * From BancoSistemaVendas..TbVendas
+Select * From BancoSistemaVendas..TbProdutos
+Select * From BancoSistemaVendas..TbVendedor
+
 -- Relação de Dependentes por Vendedor
+-- Retornar informações: Nome do Vendedor e do Dependente, Tempo de Empresa do Vendedor, 
+-- Idade e Sexo do Dependente
+Select * From BancoSistemaVendas..TbVendedor
+Select * From BancoSistemaVendas..TbDependente
+Select * From BancoSistemaVendas..TbPessoa
+
+Select Ven.Vendedor
+     , TempoEmpresa = DATEDIFF(year, Pes.DtAdmissao, getdate())
+     , Dep.Depedente
+     , Sexo = CASE WHEN Dep.Sexo = 1 THEN 'M' 
+	               WHEN Dep.Sexo = 2 THEN 'F'
+	          ELSE 'Erro' END
+	 , Idade = DATEDIFF(year, Dep.DtNasc, getdate())
+  From BancoSistemaVendas..TbVendedor   Ven
+  join BancoSistemaVendas..TbDependente Dep on Dep.CodigoVendedor = Ven.CodigoVendedor
+  join BancoSistemaVendas..TbPessoa     Pes on Pes.CodigoPessoa   = Ven.CodigoPessoa
+ Order by Ven.CodigoVendedor
 
 ------------------------
 -- Qualidade de Dados --
@@ -199,17 +225,24 @@ Select Ven.Vendedor, Pes.Sexo, Dep.Depedente, Dep.Sexo
 Select Ven.Vendedor
      , Pes.Sexo
      , Dep.Depedente
-     , CASE WHEN Dep.Sexo = 1 THEN 'M' ELSE 'F' END
+     , CASE WHEN Dep.Sexo = 1 THEN 'M' 
+	        WHEN Dep.Sexo = 2 THEN 'F'
+	   ELSE 'Erro' END
   From TbVendedor   Ven
   Join TbPessoa     Pes ON Pes.CodigoPessoa   = Ven.CodigoPessoa
   Join TbDependente Dep ON Dep.CodigoVendedor = Ven.CodigoVendedor
  Order by Ven.CodigoVendedor
+
+Select * From TbDependente
+
+-- Ajustando no banco de dados
+--Update TbDependente
+--Set    Sexo = 1
+--Where  CodigoDependente = 10
  
-
- --------------------
--- Dicas / Dúvidas --
----------------------
-
+ --------------------------------------
+-- Dicas de comandos mais utilizados --
+---------------------------------------
 -- Criando tabela de Teste
 CREATE TABLE TbTeste (
    CodigoPessoa int identity(1,1) primary key
@@ -245,13 +278,58 @@ INSERT INTO TbTeste VALUES ('Teste', 'H', '20190311')
 
 INSERT INTO TbTeste VALUES ('Teste', 'M', '20191311')
 
--- SELECT
--- WHERE
--- MAIOR e MENOR QUE
--- DELETE
--- UPDATE
+-- Admitidos a partir de 2017
+Select * 
+  From TbTeste
+ Where DtAdmissao >= '20170101'
 
----------------------------------------
--- Um pouco do que veremos amanhã... --
----------------------------------------
--- PowerBI
+-- Admissões entre os anos de 2017 e 2018
+Select * 
+  From TbTeste
+ Where DtAdmissao between '20170101' and '20181231'
+
+-- Admissões por Ano
+Select Ano = datepart(year, DtAdmissao)
+     , Qtd = Count(*)
+  From TbTeste
+ Group by datepart(year, DtAdmissao)
+
+-- Deletar todos os funcionarios admitidos em 2015
+Select * From TbTeste
+
+Delete From TbTeste
+ Where datepart(year, DtAdmissao) = 2015
+
+-- Atualizar o nome do funcionario (Pessoa) de código 8, para Alex Souza
+Select * From TbTeste
+
+begin transaction
+
+Update TbTeste
+Set    Pessoa = 'Alex Souza'
+Where CodigoPessoa = 8
+
+rollback
+
+-- Limpar os dados da tabela
+Select * From TbTeste
+
+-- Deletando os dados da Tabela
+Delete from TbTeste
+
+-- Inserir mais dados
+INSERT INTO TbTeste VALUES ('Alex Souza'    , 'M', '20150512')
+INSERT INTO TbTeste VALUES ('Antonio Souza' , 'M', '20150515')
+
+-- Deletar dados e "Reiniciar" a numeração
+Truncate table TbTeste
+
+-- Inserir novamente
+INSERT INTO TbTeste VALUES ('Alex Souza'    , 'M', '20150512')
+INSERT INTO TbTeste VALUES ('Antonio Souza' , 'M', '20150515')
+
+Select * From TbTeste
+
+---------------------------
+-- Vamos agora para o BI --
+---------------------------
